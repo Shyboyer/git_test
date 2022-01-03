7fbe48ea088f552d7c7407dfb7d6d2f6b7fdec3f
shyboyer_glo <shyboyer_glo@gmail.com>
2022-01-03 23:13:13 +0800

    push test1

--- src/main/java/com/qingfeng/git/test/GitTest.java
+++ src/main/java/com/qingfeng/git/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello World4!");
         System.out.println("master text!");
         System.out.println("hot fix test1!");
+        System.out.println("push  test1!");
     }
 }
