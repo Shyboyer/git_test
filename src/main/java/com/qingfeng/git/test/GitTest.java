d562bfd1b8416faed57564cb7541fe5c08015fdb
shyboyer_glo <shyboyer_glo@gmail.com>
2022-01-03 21:25:28 +0800

    second  commit

--- src/main/java/com/qingfeng/git/test/GitTest.java
+++ src/main/java/com/qingfeng/git/test/GitTest.java
@@ -3,5 +3,6 @@ package com.qingfeng.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello World!");
+        System.out.println("hello World2!");
     }
 }
